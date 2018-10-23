376166ac2faf8b7b64e894e1f9f60ff86196d90d
Michal.Polinkiewicz <Michal.Polinkiewicz@empirica.pl>
2018-10-23 15:26:22 +0200

    config changes

--- src/main/java/pl/springAndHibernate/config/PersistenceConfig.java
+++ src/main/java/pl/springAndHibernate/config/PersistenceConfig.java
@@ -8,12 +8,14 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
 
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
@@ -22,20 +24,32 @@ import java.util.Properties;
 @Configuration
 @EnableTransactionManagement
 @EnableJpaRepositories(basePackages = "pl.springAndHibernate")
-@PropertySource("classpath:hibernate-prod.properties")
-@Profile("prod")
+@PropertySource("classpath:hibernate.properties")
 public class PersistenceConfig {
 
     @Autowired
     private Environment environment;
 
     @Bean
-    public DataSource getDataSource() {
+    @Profile("prod")
+    public DataSource getDataSourceProd() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName(environment.getProperty("driver"));
-        dataSource.setUrl(environment.getProperty("url"));
-        dataSource.setUsername(environment.getProperty("user"));
-        dataSource.setPassword(environment.getProperty("password"));
+        dataSource.setDriverClassName(environment.getProperty("mysql.driver"));
+        dataSource.setUrl(environment.getProperty("mysql.url"));
+        dataSource.setUsername(environment.getProperty("mysql.user"));
+        dataSource.setPassword(environment.getProperty("mysql.password"));
+
+        return dataSource;
+    }
+
+    @Bean
+    @Profile("test")
+    public DataSource getDataSourceTest() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(environment.getProperty("h2.driver"));
+        dataSource.setUrl(environment.getProperty("h2.url"));
+        dataSource.setUsername(environment.getProperty("h2.user"));
+        dataSource.setPassword(environment.getProperty("h2.password"));
 
         return dataSource;
     }
@@ -43,10 +57,25 @@ public class PersistenceConfig {
     @Bean
     public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
         LocalContainerEntityManagerFactoryBean entityManager = new LocalContainerEntityManagerFactoryBean();
-        entityManager.setDataSource(getDataSource());
+        entityManager.setDataSource(getDataSourceTest());
         entityManager.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
         entityManager.setPackagesToScan("pl.springAndHibernate.dao");
-        entityManager.setJpaProperties(properties());
+
+        String [] activeProfiles = environment.getActiveProfiles();
+        String activeProfile = "";
+
+        for (int i = 0; i < activeProfiles.length; i++){
+            if (activeProfiles[i].equals("prod")){
+                activeProfile = activeProfiles[i];
+            }
+        }
+
+        if (activeProfile.equals("prod")){
+            entityManager.setJpaProperties(propertiesProd());
+        } else {
+            entityManager.setJpaProperties(propertiesTest());
+        }
+
 
         return entityManager;
     }
@@ -60,12 +89,25 @@ public class PersistenceConfig {
     }
 
     @Bean
-    public Properties properties(){
+    @Profile("prod")
+    public Properties propertiesProd() {
         Properties properties = new Properties();
-        properties.setProperty("hibernate.dialect", environment.getProperty("dialect"));
-        properties.setProperty("hibernate.show_sql", environment.getProperty("show_sql"));
-        properties.setProperty("hibernate.format_sql", environment.getProperty("format_sql"));
-        properties.setProperty("hibernate.hbm2ddl.auto", environment.getProperty("auto"));
+        properties.setProperty("hibernate.dialect", environment.getProperty("mysql.dialect"));
+        properties.setProperty("hibernate.show_sql", environment.getProperty("mysql.show_sql"));
+        properties.setProperty("hibernate.format_sql", environment.getProperty("mysql.format_sql"));
+        properties.setProperty("hibernate.hbm2ddl.auto", environment.getProperty("mysql.auto"));
+
+        return properties;
+    }
+
+    @Bean
+    @Profile("test")
+    public Properties propertiesTest() {
+        Properties properties = new Properties();
+        properties.setProperty("hibernate.dialect", environment.getProperty("h2.dialect"));
+        properties.setProperty("hibernate.show_sql", environment.getProperty("h2.show_sql"));
+        properties.setProperty("hibernate.format_sql", environment.getProperty("h2.format_sql"));
+        properties.setProperty("hibernate.hbm2ddl.auto", environment.getProperty("h2.auto"));
 
         return properties;
     }
--- src/test/groovy/pl/springAndHibernate/dao/DaoSpec.groovy
+++ src/test/groovy/pl/springAndHibernate/dao/DaoSpec.groovy
@@ -1,18 +1,27 @@
 package pl.springAndHibernate.dao
 
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.core.env.Environment
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.ContextConfiguration
 import pl.springAndHibernate.config.PersistenceConfig
-import pl.springAndHibernate.config.PersistenceTestConfig
+
 import spock.lang.Specification
 
-@ContextConfiguration(classes = [PersistenceConfig.class, PersistenceTestConfig.class])
+@ContextConfiguration(classes = [PersistenceConfig.class])
 @ActiveProfiles("test")
 class DaoSpec extends Specification {
 
     @Autowired
+    private Environment environment
+    @Autowired
     CatRepository catRepository
+
+    def "active profile should be test"(){
+        expect:
+        environment.activeProfiles.length == 1
+        environment.getActiveProfiles()[0] == "test"
+    }
 
     def "save() should persist object in db"() {
         given:
